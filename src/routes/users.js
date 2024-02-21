ac2ad66afd2f3368d0eb436c4bb6600be556fd2e
Miguel Prot <prot.garcia@gmail.com>
2024-02-21 02:53:21 -0500

    validations

--- src/routes/users.js
+++ src/routes/users.js
@@ -24,15 +24,9 @@ routes.put('/:id', [
     check('name')
         .isString()
         .isLength({min: 3, max: 25}),
-    check('birthday')
-        .matches(/^\d{4}-\d{2}-\d{2}$/)
-        .withMessage('The date format should be YYYY-MM-DD'),
     check('email')
         .isEmail()
         .withMessage('Please insert a valid email'),
-    check('phone')
-        .matches(/^\+(?:[0-9] ?){6,14}[0-9]$/).
-        withMessage('Please insert a valid phone number for example +529984834032')
 ], isAuthenticated, users.updateUser);
 routes.delete('/:id', isAuthenticated, users.deleteUser);
 
