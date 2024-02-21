d416b1a4111aa1a48185f2abc1bb7decd487abeb
gislpzbyu <64442761+gislpzbyu@users.noreply.github.com>
2024-02-20 21:52:28 -0600

    Giselle testing merge

--- src/controllers/NotesControllers.js
+++ src/controllers/NotesControllers.js
@@ -26,6 +26,23 @@ const getNote = async (req, res) => {
     }
 };
 
+const getNoteById = async (req, res) => {
+    const { id: noteId } = req.params;
+
+    try {
+        if (noteId) {
+            let existingNote = await mongodb.getDatabase().db().collection(collection).findOne({ _id: new ObjectId(noteId) });
+            res.status(200).json(existingNote);
+        }
+    } catch (err) {
+        console.log(err instanceof Error);
+        console.log('ERROR err', err.message);
+        res.status(404).json({
+            message: "NOT FOUND"
+        });
+    }
+}
+
 const createNote = async (req, res) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
@@ -59,22 +76,14 @@ const createNote = async (req, res) => {
 const updateNote = async (req, res) => {
     try {
         const id = req.params.id;
-        const note = {
-            title: req.body.title,
-            description: req.body.description,
-            category: req.body.category,
-            priority: req.body.priority,
-            status: req.body.status,
-            dueDate: req.body.dueDate,
-            assignedTo: req.body.assignedTo
-        };
+
 
         const errors = validationResult(req);
         if (!errors.isEmpty()) {
             return res.status(422).json({ errors: errors.array() });
         }
 
-        const response = await mongodb.getDatabase().db().collection(collection).updateOne({ _id: new ObjectId(id) }, { $set: note });
+        const response = await mongodb.getDatabase().db().collection(collection).updateOne({ _id: new ObjectId(id) }, { $set: req.body });
 
         if (response.acknowledged) {
             res.status(200).json(response);
@@ -88,19 +97,27 @@ const updateNote = async (req, res) => {
 };
 
 const deleteNote = async (req, res) => {
-    const id = new ObjectId(req.params.id);
-    const response = await mongodb.getDatabase().db().collection(collection).deleteOne({ _id: id }, true);
+    try {
+        const id = new ObjectId(req.params.id);
+        const response = await mongodb.getDatabase().db().collection(collection).deleteOne({ _id: id }, true);
 
-    if (response.deletedCount > 0) {
-        res.status(200).send('Note deleted');
-    } else {
-        res.status(500).json(response.error || 'There was an error deleting the note');
+        if (response.deletedCount > 0) {
+            res.status(200).send('Note deleted');
+        } else {
+            res.status(500).json(response.error || 'There was an error deleting the note');
+        }
+    } catch (err) {
+        res.status(500).json({
+            error: err.message
+        });
     }
 };
 
 module.exports = {
     getNote,
+    getNoteById,
     createNote,
     updateNote,
     deleteNote
 };
+
--- src/routes/note.js
+++ src/routes/note.js
@@ -1,57 +1,14 @@
 const express = require('express');
-const note = require('../controllers/NotesController');
+const note = require('../controllers/NotesControllers');
 const routes = express.Router();
-const { check } = require('express-validator');
+
 const { isAuthenticated } = require('../middleware/authenticate');
+const { newNoteValidation, updateNoteValidation  } = require('../middleware/notesValidation');
 
 routes.get('/', isAuthenticated, note.getNote);
-routes.get('/:id', isAuthenticated, note.getNote);
-routes.post('/', [
-    check('title')
-        .isString()
-        .isLength({ min: 3, max: 25 }),
-    check('description')
-        .isString()
-        .isLength({ max: 255 }),
-    check('category')
-        .isString()
-        .isLength({ max: 50 }),
-    check('priority')
-        .isString()
-        .isIn(['low', 'medium', 'high']),
-    check('status')
-        .isString()
-        .isIn(['pending', 'completed']),
-    check('dueDate')
-        .matches(/^\d{4}-\d{2}-\d{2}$/)
-        .withMessage('The date format should be YYYY-MM-DD'),
-    check('assignedTo')
-        .isString()
-        .isLength({ max: 50 })
-], isAuthenticated, note.createNote);
-routes.put('/:id', [
-    check('title')
-        .isString()
-        .isLength({ min: 3, max: 25 }),
-    check('description')
-        .isString()
-        .isLength({ max: 255 }),
-    check('category')
-        .isString()
-        .isLength({ max: 50 }),
-    check('priority')
-        .isString()
-        .isIn(['low', 'medium', 'high']),
-    check('status')
-        .isString()
-        .isIn(['pending', 'completed']),
-    check('dueDate')
-        .matches(/^\d{4}-\d{2}-\d{2}$/)
-        .withMessage('The date format should be YYYY-MM-DD'),
-    check('assignedTo')
-        .isString()
-        .isLength({ max: 50 })
-], isAuthenticated, note.updateNote);
+routes.get('/:id', isAuthenticated, note.getNoteById);
+routes.post('/', newNoteValidation, isAuthenticated, note.createNote);
+routes.put('/:id', updateNoteValidation, isAuthenticated, note.updateNote);
 routes.delete('/:id', isAuthenticated, note.deleteNote);
 
 module.exports = routes;
